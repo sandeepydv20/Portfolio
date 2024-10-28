744cead63eb21c057a5199f23efb6a96ece4a987
Sandeep yadav <ggnydv2000@gmail.com>
2024-10-28 14:48:23 +0530

    validation add

--- src/script.js
+++ src/script.js
@@ -1,54 +1,119 @@
-function menubar(){
-    document.getElementById("menu").classList.toggle('h-[100vh]');
-    document.getElementById("menu").classList.toggle('h-16');
-    document.querySelector('ul').classList.toggle('mt-[8rem]');
-    document.querySelector('ul').classList.toggle("opacity-100");
-    document.querySelector('ul').classList.toggle("w-[49.5%]");
-    document.querySelector('ul').classList.toggle("item-center");
-    // document.querySelector('ul').classList.toggle("teyt-md");
-    document.getElementById("mode-changer").classList.toggle('hidden');
-    
 
+function menubar(){ 
+    navslide();
+    hamburger();
+    var navLinks = document.querySelectorAll('.nav-link')
+    navLinks.forEach(e => {
+        e.addEventListener('click',() => {      
+           menubar()  
+        })
+        }) 
+
+}
+
+function navslide(){
+    document.getElementById("menu").classList.toggle('h-screen');
+    document.querySelector('ul').classList.toggle("opacity-100");
+    document.getElementById("mode-changer").classList.toggle('hidden');
+}
+
+ function hamburger(){
     document.getElementById('line_1').classList.toggle('rotate-45')
     document.getElementById('line_1').classList.toggle('mt-[16px]')
     document.getElementById('line_2').classList.toggle('rotate-[-45deg]')
     document.getElementById('line_2').classList.toggle('mt-[-5px]')
     document.getElementById('line_3').classList.toggle('hidden')
-}
+ }
+ 
 
-// // const navLinks = document.querySelectorAll('.nav-link');
-// // navLinks.forEach(link => {
-// //     link.addEventListener('click', function() {
-// //         const navbar = document.getElementById('menu');
-// //         navbar.classList.remove('active'); // Close nav on link click
-// //     });
-// // });
+   
+
+
+
 
 
 function Mode(){
    
-    document.getElementById('menu-1').classList.toggle('bg-[grey]');
+    document.getElementById('menu-1').classList.toggle('backdrop-blur-2xl');
     document.querySelector('div').classList.toggle('bg-[silver]/90');
     document.querySelector('div').classList.toggle('text-[black]');
+    document.getElementById('hemClick').classList.toggle('border-[black]');
+    document.getElementById('line_1').classList.toggle('bg-[black]');
+    document.getElementById('line_2').classList.toggle('bg-[black]');
+    document.getElementById('line_3').classList.toggle('bg-[black]');
   let changecolor = document.getElementsByClassName('bg-[#1C2326]');
-     
    for (const change of changecolor) {
     change.classList.toggle('bg-[grey]');
-   }
-       
-    
-    
-
-   
+   } 
 }
 
-// gsap.from("#logo",{
-//     y:-200,
-//     opacity:0,
-//     duration:1,
-//     ease:''
-// }
-// )
+const name =document.getElementById('name');
+ Name = (e) => {
+e.name.v
+}
+
+const form = document.getElementById('contact_me');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const subjectInput = document.getElementById('subject');
+// const message = document.getElementById('message');
+const errorname = document.getElementById('errorname');
+const erroremail = document.getElementById('erroremail');
+const errorsubject = document.getElementById('errorsubject');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    const name =nameInput.value.trim();
+
+    const nameRegex =/^[A-Za-z\s]+$/;
+
+    if (name==='' || !nameRegex.test(name)) {
+        errorname.classList.remove('hidden');
+    }
+    else{
+        errorname.classList.add('hidden');
+      
+    }
+
+    const email = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (email==='' || !emailRegex.test(email)) {
+        erroremail.classList.remove('hidden');
+    }
+    else{
+        erroremail.classList.add('hidden');
+    }
+
+    const subject= subjectInput.value.trim();
+    if (subject==='') {
+        errorsubject.classList.remove('hidden');
+    }
+    else{
+        errorsubject.classList.add('hidden');
+      
+         
+    }
+    if (name && email && subject) {
+        alert('Your request succesfully registered: Mr/Mrs  '+ name);
+    }
+    else{
+        alert('Your request not registered: !!! ,Please fill your detail...');
+    }
+   
+})
+
+
+
+gsap.from("#logo",{
+    y:-200,
+    x:100,
+    opacity:0,
+    duration:2,
+    ease:'bounce'
+}
+)
 // // // section part-1
 
 gsap.from("#dark",{
@@ -75,7 +140,7 @@ gsap.from("#dark_2",{
         trigger:"#dark_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
     
 })
@@ -87,7 +152,7 @@ gsap.from("#image_2",{
         trigger:"#image_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     } 
 })
 // // section part-2 end
@@ -101,7 +166,7 @@ gsap.from("#services_1",{
         trigger:"#services_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
     
 })
@@ -113,7 +178,7 @@ gsap.from("#services_2",{
         trigger:"#services_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 // // section part-3 end
@@ -128,7 +193,7 @@ gsap.from("#services_card_1",{
         trigger:"#services_card_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 gsap.from("#services_card_2",{
@@ -139,7 +204,7 @@ gsap.from("#services_card_2",{
         trigger:"#services_card_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 gsap.from("#services_card_3",{
@@ -150,7 +215,7 @@ gsap.from("#services_card_3",{
         trigger:"#services_card_3",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 
@@ -167,7 +232,7 @@ gsap.from("#portfolio_1",{
         trigger:"#portfolio_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
     
 })
@@ -179,7 +244,7 @@ gsap.from("#portfolio_2",{
         trigger:"#portfolio_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 
@@ -195,7 +260,7 @@ gsap.from("#portfolio_card_1",{
         trigger:"#portfolio_card_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#portfolio_card_2",{
@@ -206,7 +271,7 @@ gsap.from("#portfolio_card_2",{
         trigger:"#portfolio_card_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#portfolio_card_3",{
@@ -217,7 +282,7 @@ gsap.from("#portfolio_card_3",{
         trigger:"#portfolio_card_3",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 
@@ -233,7 +298,7 @@ gsap.from("#portfolio_card_4",{
         trigger:"#portfolio_card_4",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#portfolio_card_5",{
@@ -244,7 +309,7 @@ gsap.from("#portfolio_card_5",{
         trigger:"#portfolio_card_5",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#portfolio_card_6",{
@@ -255,7 +320,7 @@ gsap.from("#portfolio_card_6",{
         trigger:"#portfolio_card_6",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 // portfolio card 2 part  end
@@ -270,7 +335,7 @@ gsap.from("#blogs_1",{
         trigger:"#blogs_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
     
 })
@@ -282,7 +347,7 @@ gsap.from("#blogs_2",{
         trigger:"#blogs_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 //blogs part end
@@ -296,7 +361,7 @@ gsap.from("#blogs_card_1",{
         trigger:"#blogs_card_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#blogs_card_2",{
@@ -307,7 +372,7 @@ gsap.from("#blogs_card_2",{
         trigger:"#blogs_card_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 gsap.from("#blogs_card_3",{
@@ -318,7 +383,18 @@ gsap.from("#blogs_card_3",{
         trigger:"#blogs_card_3",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
+    }  
+})
+gsap.from("#blogs_card_4",{
+    y:200,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#blogs_card_4",
+        scroller:"body",
+        // markers:true,
+        start:"top 85%"
     }  
 })
 //blogs card end
@@ -333,7 +409,7 @@ gsap.from("#contact_1",{
         trigger:"#contact_1",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
     
 })
@@ -345,7 +421,7 @@ gsap.from("#contact_2",{
         trigger:"#contact_2",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }
 })
 //contact part end
@@ -357,7 +433,7 @@ gsap.from("#contact_me",{
         trigger:"#contact_me",
         scroller:"body",
         // markers:true,
-        start:"top 80%"
+        start:"top 85%"
     }  
 })
 //contact_me part end
